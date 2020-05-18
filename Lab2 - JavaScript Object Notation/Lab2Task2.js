e75e04565f99a0aa0a5a552e1fea0375ebd2d71c
khai cher <lim.khaicher@gmail.com>
2020-05-18 17:58:18 +0800

    added soln for Lab1-7

---
+++ Lab2 - JavaScript Object Notation/Lab2Task2.js
@@ -0,0 +1,21 @@
+var square = {
+    length: 5,
+    color: "red",
+    getArea: function() {
+        return this.length * this.length;
+    }
+}
+console.log(square.getArea());
+
+var square2 = {
+    length: 5,
+    color: "red",
+    getArea: function() {
+        return this.length * this.length;
+    },
+    compare: function(sq) {
+        return (this.length==sq.length && this.color==sq.color);
+    } 
+}
+
+console.log(square2.compare(square));
---
+++ Lab7 - Node JS Web Application I/routes.js
@@ -0,0 +1,47 @@
+var bodyParser = require('body-parser');
+var eventsController = require('./controllers/eventsController.js');
+
+var routes = function () {
+    var router = require('express').Router();
+    
+    router.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    router.get('/', function(req, res) {
+        res.sendFile(__dirname+"/views/index.html");
+    });
+
+    router.get('/css/*', function(req, res)  {
+        res.sendFile(__dirname+"/views/"+req.originalUrl);
+    });
+    
+    router.get('/js/*', function(req, res)  {
+        res.sendFile(__dirname+"/views/"+req.originalUrl);
+    });
+
+    router.get('/events',function(req,res){
+        res.send(eventsController.getEvents());
+    })
+    router.post('/events', function(req, res) {
+        var data = req.body;
+        
+        var event = {
+            name: data.name,
+            description: data.description,
+            start: {
+                date: data.startDate,
+                time: data.startTime
+            },
+            end: {
+                date: data.endDate,
+                time: data.endTime
+            }
+        };
+        
+        eventsController.addEvent(event);     
+        res.redirect('back');        
+    });
+    return router;
+};
+
+module.exports = routes();
